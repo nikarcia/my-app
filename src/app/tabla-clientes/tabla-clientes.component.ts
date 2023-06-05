8420f017278b632929c0385ec688664677ea595c
nikarcia <nicolasgarcia612@gmail.com>
2023-06-05 00:00:21 -0500

    first commit

---
+++ src/app/tabla-clientes/tabla-clientes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-tabla-clientes',
+  templateUrl: './tabla-clientes.component.html',
+  styleUrls: ['./tabla-clientes.component.css']
+})
+export class TablaClientesComponent implements OnInit {
+  clientes: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url = 'http://localhost:8080/api/cliente'; // Reemplaza con la URL correcta de tu API REST para obtener la lista de clientes
+
+    this.http.get<any>(url).subscribe(
+      response => {
+        this.clientes = response;
+        console.log('Clientes obtenidos exitosamente', this.clientes);
+      },
+      error => {
+        console.error('Error al obtener la lista de clientes', error);
+      }
+    );
+  }
+}
